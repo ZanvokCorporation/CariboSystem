9d74ba3456e48fccc3a35c0b633acc9cec94aff2
Zanvok Corporation <74700000+ZanvokCorporation@users.noreply.github.com>
2022-03-12 13:59:24 +0530

    v6.04

--- calc.py
+++ calc.py
@@ -1,46 +1,59 @@
-print("Calculator for CariboSystem")
+import os
+import sys
+import subprocess
+
 print()
-print("~"*45)
+print("Calc for CariboSystem")
 print()
-print("Available Operands:-")
-print("""
-    \t 1. Addition
-    \t 2. Subtraction
-    \t 3. Multiplication
-    \t 4. Division
-    \t 5. Square Root
-""")
-print("Type the number or operand name to continue" , " Type exit to quit")
+
 calc = ""
-while calc != "quit":
-    calc = input("Calc> ")
-    if calc == 1 or calc == "add" or calc == "addition" or calc == "sum" or calc == "+":
-        add1 = input("Enter first number: ")
-        add2 = input("Enter second number: ")
-        add = float(add1)+float(add2)
-        print("Your answer is: " , add)
-    elif calc == 2 or calc == "sub" or calc == "subtraction" or calc == "subtract" or calc == "minus" or calc == "-":
-        sub1 = input("Enter first number: ")
-        sub2 = input("Enter second number: ")
-        sub = float(sub1)-float(sub2)
-        print("Your answer is: " , sub)
-    elif calc == 3 or calc == "multiplication" or calc == "multiply" or calc == "X" or calc == "*" or calc == "mul":
-        mul1 = input("Enter first number: ")
-        mul2 = input("Enter second number: ")
-        mul = float(mul1)*float(mul2)
-        print("Your answer is: " , mul)
-    elif calc == 4 or calc == "division" or calc == "divide" or calc == "div" or calc == "/":
-        div1 = input("Enter first number: ")
-        div2 = input("Enter second number: ")
-        div = float(div1)/float(div2)
-        print("Your answer is: " , div)
-    elif calc == 5 or calc == "Square Root" or calc == "SquareRoot" or calc == "sqroot":
-        sqroot_number = input("Enter number: ")
-        sqroot = float(sqroot_number)*float(sqroot_number)
-        print("Your answer is: " , sqroot)
-    elif calc == "exit":
-        from switch import *
-    elif calc == "about":
-        print("Calc for cariboSystem 6")
+
+while calc != "quit" or calc != "exit":
+
+    calc = input("CariboSystem > Calc > ").lower()
+
+    if calc == "+" or calc == "add":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        add = num1+num2
+        print("Sum: ", add)
+
+    elif calc == "-" or calc == "sub":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        sub = num1-num2
+        print("Sub: ", sub)
+
+    elif calc == "*" or calc == "mul":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        mul = num1*num2
+        print("Mul: ", mul)
+
+    elif calc == "/" or calc == "div":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        div = num1/num2
+        print("Div: ", div)
+
+    elif calc == "exit" or calc == "quit":
+        print()
+        break
+
+    elif calc == "cls":
+        os.system('cls')
+
+    elif calc == "clear":
+        os.system('clear')
+
+    elif calc == "ver" or calc == "version":
+        ver = 6.00
+        os_release = "CariboSystem6"
+        r_ty = "Beta"
+        print()
+        print("Calc(CariboSystem6)6.0BETA")
+        print()
+
     else:
-        print("Invalid calc command!")
+        print("Command " + calc + " not found...")
+    
---
+++ caribosystem.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import subprocess
+
+print()
+ver = 6.0
+r_type = "BETA"
+print()
+
+core = ""
+if sys.platform == "win32":
+    os.system('cls')
+else:
+    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    os.system('clear')
+
+while core != "quit" or core != "exit":
+
+    core = input("CariboSystem > ")
+
+    if core == "ver":
+        print()
+        print("CariboSystem")
+        print("6.04")
+        print("Zanvok Corporation")
+        print()
+
+    elif core == "ver core":
+        print()
+        print("Core(CariboSystem6).6.0Beta")
+        print()
+
+    elif core == 'calc':
+        if sys.platform == "win32":
+            os.system('python calc.py')
+        else:
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
+            os.system('python3 calc.py')
+
+    elif core == 'cls':
+        os.system('cls')
+
+    elif core == 'clear':
+        os.system('clear')
+
+    elif core == "exit" or core == "quit":
+        print()
+        print("GoodBye!")
+        print()
+        break
+
+    else:
+        print()
+        print("Bad Command..")
+        print()
